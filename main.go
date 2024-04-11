d38046e1cb74afcb04bc0bd8321fbff51f3866b5
BH3Panzer <clementcattin74@gmail.com>
2024-04-11 19:34:46 +0200

    6000 fps limit and fps showed

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"strconv"
+)
 
 func main() {
 	const width int32 = 1280
@@ -9,9 +12,11 @@ func main() {
 	rl.InitWindow(width, height, "Ping Pong Pun")
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(60)
+	rl.SetTargetFPS(6000)
 
 	rl.ToggleFullscreen()
+
+	rl.HideCursor()
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
@@ -19,6 +24,10 @@ func main() {
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Ping Pong Pun", center_title, 20, 50, rl.Gray)
 		rl.DrawText("Press enter to start", center_title, 100, 20, rl.Gray)
+		fps := rl.GetFPS()
+		var converted_fps = int(fps)
+
+		rl.DrawText("fps: "+strconv.Itoa(converted_fps), 10, 10, 20, rl.Black)
 
 		rl.EndDrawing()
 	}
