ca2ef2888e6ec8ac0ac304064428d6082220de7c
BH3Panzer <clementcattin74@gmail.com>
2024-04-11 19:11:35 +0200

    fixed resolution

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	const width int32 = 1280
+	const height int32 = 720
+	const center_title int32 = width/3
+	rl.InitWindow(width, height, "Ping Pong Pun")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
+
+	rl.ToggleFullscreen()
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Ping Pong Pun", 190, 20, 50, rl.Gray)
-		rl.DrawText("Press enter to start", 190, 100, 20, rl.Gray)
+		rl.DrawText("Ping Pong Pun", center_title, 20, 50, rl.Gray)
+		rl.DrawText("Press enter to start", center_title, 100, 20, rl.Gray)
 
 		rl.EndDrawing()
 	}
